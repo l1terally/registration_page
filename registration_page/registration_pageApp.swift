c80b84385ba718be12e9abd7ab23122e3946712a
l1terally <literallyuriy@gmail.com>
2023-01-03 17:40:09 +0800

    Initial Commit

---
+++ registration_page/registration_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  registration_pageApp.swift
+//  registration_page
+//
+//  Created by Юрий  Лизунов on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct registration_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
