196897c37f7d37649cebb68b2a9abd1c201eeb44
l1terally <literallyuriy@gmail.com>
2023-01-05 20:28:16 +0800

    Start page created

--- registration_page/ContentView.swift
+++ registration_page/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var index = 0
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader{ _ in
+            VStack{
+                Image("foxx")
+                    .resizable()
+                    .frame(width: 70, height: 70)
+                
+                ZStack{
+                    SignUp(index: self.$index)
+                        .zIndex(Double(self.index))
+                    Login(index: self.$index)
+                }
+                
+                HStack(spacing: 15) {
+                    Rectangle()
+                        .fill(Color("main"))
+                        .frame(height: 1)
+                    
+                    Text("OR")
+                    
+                    Rectangle()
+                        .fill(Color("main"))
+                        .frame(height: 1)
+                }.padding(.horizontal, 30)
+                    .padding(.top, 50)
+                
+                HStack(spacing: 25) {
+                    Button(action: {
+                        //
+                    }) {
+                        Image("apple")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 60)
+                        
+                    }
+                    Button(action: {
+                        //
+                    }) {
+                        Image("insta")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                        
+                    }
+                    .offset(y: 5)
+                    Button(action: {
+                        //
+                    }) {
+                        Image("google")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 55)
+                            
+                    }
+                    .offset(y: 4)
+                }
+                .padding(.top, 30)
+            }
+            .padding(.vertical)
+            
         }
-        .padding()
+        .background(Color(.black))
+        .preferredColorScheme(.dark)
     }
 }
 
@@ -24,3 +86,218 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        return Path { path in
+            path.move(to: CGPoint(x: rect.width, y: 100))
+            path.addLine(to: CGPoint(x:rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: 0))
+        }
+    }
+}
+
+
+struct CShape1: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        return Path { path in
+            path.move(to: CGPoint(x: 0, y: 100))
+            path.addLine(to: CGPoint(x:0, y: rect.width))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+        }
+    }
+}
+
+
+struct Login: View {
+    @State var email = ""
+    @State var pass = ""
+    
+    @Binding var index : Int
+    
+    
+    var body: some View{
+        ZStack(alignment: .bottom) {
+            VStack{
+                HStack{
+                    VStack(spacing: 10) {
+                        Text("Login")
+                            .foregroundColor(self.index == 0 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Capsule()
+                            .fill(self.index == 0 ? Color.blue : Color.clear)
+                            .frame(width: 100, height: 5)
+                    }
+                    Spacer()
+                } .padding(.top, 32)
+                    
+                
+                VStack{
+                    HStack(spacing: 15){
+                        Image(systemName: "envelope.fill")
+                            .foregroundColor(Color("main"))
+                        TextField("Email adress", text: self.$email)
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                } .padding(.horizontal)
+                    .padding(.top, 40)
+                
+                VStack{
+                    HStack{
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("main"))
+                        SecureField("Password", text: self.$pass)
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                } .padding(.horizontal)
+                    .padding(.top, 30)
+                
+                HStack{
+                    Spacer(minLength: 0)
+                    Button(action: {
+                        //
+                    }) {
+                        Text("Forget Password?")
+                            .foregroundColor(Color.white.opacity(0.6))
+                    }
+                } .padding(.horizontal)
+                    .padding(.top, 30)
+            } .padding()
+                .padding(.bottom, 65)
+                .background(Color("but"))
+                .clipShape(CShape())
+                .contentShape(CShape())
+                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+                .onTapGesture {
+                    self.index = 0
+                }
+                .cornerRadius(35)
+                .padding(.horizontal)
+            
+            
+            Button(action: {
+                //
+            }) {
+                Text("LOGIN")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding(.horizontal, 50)
+                    .background(Color("main"))
+                    .clipShape(Capsule())
+                    .shadow(color: Color.white.opacity(0), radius: 5, x:0, y:5)
+            }
+            .offset(y: 25)
+            .opacity(self.index == 0 ? 1 : 0)
+        }
+    }
+}
+
+
+
+struct SignUp: View {
+    @State var email = ""
+    @State var pass = ""
+    @State var RePass = ""
+    @Binding var index : Int
+    
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack{
+                HStack{
+                    Spacer(minLength: 0)
+                    
+                    VStack(spacing: 10) {
+                        Text("SignUp")
+                            .foregroundColor(self.index == 1 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Capsule()
+                            .fill(self.index == 1 ? Color.blue : Color.clear)
+                            .frame(width: 100, height: 5)
+                    }
+                }
+                
+                .padding(.top, 36)
+                
+                
+                
+                VStack{
+                    HStack(spacing: 15) {
+                        Image(systemName: "envelope.fill")
+                            .foregroundColor(Color("but"))
+                        TextField("Email Adress", text: self.$email)
+                    }
+                    Divider().background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                .padding(.top, 40)
+                
+                VStack{
+                    HStack(spacing: 15) {
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("but"))
+                        SecureField("Password", text: self.$pass)
+                        
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                } .padding(.horizontal)
+                    .padding(.top, 30)
+                
+                
+                VStack{
+                    HStack(spacing: 15) {
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("but"))
+                        SecureField("Password", text: self.$RePass)
+                        
+                    }
+                    Divider().background(Color.white.opacity(0.5))
+                }.padding(.horizontal)
+                    .padding(.top, 30)
+            }
+            .padding()
+            .padding(.bottom, 65)
+            .background(Color("main"))
+            .clipShape(CShape1())
+            .contentShape(CShape1())
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 1
+            }
+            .cornerRadius(35)
+            
+            
+            
+            Button(action: {
+                //
+            }) {
+                Text("SIGNUP")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding(.horizontal, 50)
+                    .background(Color("but"))
+                    .clipShape(Capsule())
+                    .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: -5)
+            }
+            .offset(y: 25)
+            .opacity(self.index == 1 ? 1 : 0)
+        }
+    }
+}
+
+
+
+
